91c4dcd9f9216744c0b1a365cdb703b6f38e32f8
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-05-29 17:41:19 +0200

    cards

---
+++ script.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+	//Loading cards
+	const cards = [
+		{
+			name: "ganhou",
+			img: "images/ganhou.png",
+		},
+		{
+			name: "ganhou",
+			img: "images/ganhou.png",
+		},
+		{
+			name: "direita",
+			img: "images/direita.png",
+		},
+		{
+			name: "direita",
+			img: "images/direita.png",
+		},
+		{
+			name: "esquerda",
+			img: "images/esquerda.png",
+		},
+		{
+			name: "esquerda",
+			img: "images/esquerda.png",
+		},
+		{
+			name: "correndo",
+			img: "images/correndo.png",
+		},
+		{
+			name: "correndo",
+			img: "images/correndo.png",
+		},
+		{
+			name: "tras",
+			img: "images/tras.png",
+		},
+		{
+			name: "tras",
+			img: "images/tras.png",
+		},
+		{
+			name: "pulo",
+			img: "images/pulo.png",
+		},
+		{
+			name: "pulo",
+			img: "images/pulo.png",
+		},
+	];
+});
