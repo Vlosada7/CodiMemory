a1fb224174b0db225e5e6c61ee59c1b0ae1c42c3
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-05-30 12:52:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 	//Loading cards
-	const cards = [
+	const cardsArray = [
 		{
 			name: "ganhou",
 			img: "images/ganhou.png",
@@ -50,4 +50,72 @@ document.addEventListener("DOMContentLoaded", () => {
 			img: "images/pulo.png",
 		},
 	];
+
+	cardsArray.sort(() => 0.5 - Math.random());
+
+	const grid = document.querySelector(".grid");
+
+	const scoreBoard = document.querySelector("#result");
+	let cardsChoosen = [];
+	let cardsChoosenId = [];
+	let pairs = [];
+
+	//Creating board
+	function createBoard() {
+		for (let i = 0; i < cardsArray.length; i++) {
+			let card = document.createElement("img");
+			card.setAttribute("src", "images/card.png");
+			card.setAttribute("data-id", i);
+			card.addEventListener("click", flipCard);
+			grid.appendChild(card);
+		}
+	}
+
+	//Checking pairs
+	function checkforMatch() {
+		let cards = document.querySelectorAll("img");
+		const optionOneId = cardsChoosenId[0];
+		const optionTwoId = cardsChoosenId[1];
+
+		//Click twice on the same card
+		if (optionOneId == optionTwoId) {
+			cards[optionOneId].setAttribute("src", "images/card.png");
+			cards[optionTwoId].setAttribute("src", "images/card.png");
+			alert("You clicked on the same image");
+		}
+		//Found a pair
+		else if (cardsChoosen[0] == cardsChoosen[1]) {
+			alert("Found a pair");
+			cards[optionOneId].setAttribute("src", "images/white.png");
+			cards[optionTwoId].setAttribute("src", "images/white.png");
+			cards[optionOneId].removeEventListener("click", flipCard);
+			cards[optionTwoId].removeEventListener("click", flipCard);
+			pairs.push(cardsChoosen);
+		}
+		//Not found a pair
+		else {
+			cards[optionOneId].setAttribute("src", "images/card.png");
+			cards[optionTwoId].setAttribute("src", "images/card.png");
+			alert("Ops! Try again");
+		}
+		cardsChoosen = [];
+		cardsChoosenId = [];
+		scoreBoard.textContent = pairs.length;
+
+		if (pairs.length == cardsArray.length / 2) {
+			scoreBoard.textContent = "Congratulation, you found all pairs";
+		}
+	}
+
+	//Flip card
+	function flipCard() {
+		let cardId = this.getAttribute("data-id");
+		cardsChoosen.push(cardsArray[cardId].name);
+		cardsChoosenId.push(cardId);
+		this.setAttribute("src", cardsArray[cardId].img);
+		if (cardsChoosen.length == 2) {
+			setTimeout(checkforMatch, 500);
+		}
+	}
+	createBoard();
 });
